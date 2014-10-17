e9f0398cdf03cf100e95e16362b7e9370fc2aa5d
thomas <vaillant.thomas@gmail.com>
2014-10-17 16:44:09 -0400

    Creation modèle

---
+++ js/QuizBd.js
@@ -0,0 +1,93 @@
+/* BD */
+function QuizBd (json) {
+    this.questions = {};
+    this.domaines = {};
+
+    for (var idQ in json.listeQuestions) {
+        // Ajout de la question à la liste globale
+        this.questions[json.listeQuestions[idQ].id] = new QuizQuestion(
+            json.listeQuestions[idQ].id,
+            json.listeQuestions[idQ].domaine,
+            json.listeQuestions[idQ].question);
+
+        // Ajout des réponses
+        for (var idR in json.listeQuestions[idQ].reponses) {
+            this.questions[json.listeQuestions[idQ].id].addReponse(
+                json.listeQuestions[idQ].reponses[idR].id,
+                json.listeQuestions[idQ].reponses[idR].value,
+                json.listeQuestions[idQ].reponses[idR].id == json.listeQuestions[idQ].idVrai
+            );
+        }
+
+        // Ajout de la question à la liste par domaine
+        if (typeof(this.domaines[json.listeQuestions[idQ].domaine]) == "undefined") {
+            this.domaines[json.listeQuestions[idQ].domaine] = []; // Si le domaine n'existe pas, on le créé
+        }
+        this.domaines[json.listeQuestions[idQ].domaine].push(this.questions[json.listeQuestions[idQ].id]);
+    }
+}
+
+QuizBd.prototype = {
+    getDomaines: function () {
+        var cles = [];
+        for (var cle in this.domaines) {
+            cles.push(cle);
+        }
+
+        return cles;
+    },
+
+    getNbQuestions: function (domaines) {
+        var nb = 0;
+        for (var k in domaines) {
+            nb += this.domaines[domaines[k]].length;
+        }
+
+        return nb;
+    },
+
+    getQuestions:  function (domaines) {
+        var questions = [];
+        for (var k in domaines) {
+            questions = questions.concat(this.domaines[domaines[k]]);
+        }
+
+        return questions;
+    },
+
+    getRandomQuestion: function (domaines, questionsPassees) {
+        var questions = this.getQuestions(domaines);
+        var questionsRestantes = [];
+
+        for (var k in questions) {
+            if (!(questions[k].id in questionsPassees)) { // Si la question n'est pas déjà passée, on l'ajoute
+                questionsRestantes.push(questions[k]);
+            }
+        }
+
+        var randomId = Math.floor(Math.random() * (questionsRestantes.length - 1));
+        return questionsRestantes[randomId];
+    }
+};
+
+/* QUESTION */
+function QuizQuestion (_id, _domaine, _question) {
+    this.id = _id;
+    this.domaine = _domaine;
+    this.question = _question;
+
+    this.reponses = {};
+}
+
+QuizQuestion.prototype = {
+    addReponse: function (id, reponse, bonne) {
+        this.reponses[id] = new QuizReponse(id, reponse, bonne);
+    }
+};
+
+/* REPONSE */
+function QuizReponse (_id, _reponse, _bonne) {
+    this.id = _id;
+    this.reponse = _reponse;
+    this.bonne = _bonne;
+}
---
+++ js/QuizUser.js
@@ -0,0 +1,65 @@
+function QuizUser () {
+//    this.questionsPassees = this.getVar("questionsPassees");
+    this.examensPasses = this.getVar("examensPasses");
+
+    if (this.questionsPassees == null) this.questionsPassees = [];
+    if (this.examensPasses == null) this.examensPasses = [];
+}
+
+QuizUser.prototype = {
+    getVar: function (variable) {
+        return JSON.parse(localStorage.getItem(variable));
+    },
+
+    setVar: function (variable, value) {
+        localStorage.setItem(variable, JSON.stringify(value));
+    },
+
+//    razQuestionsPassees: function () {
+//        this.questionsPassees = [];
+//        localStorage.removeItem("questionsPassees");
+//    },
+
+    raz: function () {
+        localStorage.clear();
+    },
+
+    save: function () {
+//        this.setVar("questionsPassees", this.questionsPassees);
+        this.setVar("examensPasses", this.examensPasses);
+    },
+
+    addQuestion: function (id, reussie) {
+        this.questionsPassees.push({
+            "id": id,
+            "reussie": reussie
+        });
+    },
+
+    addExamen: function (note, nbQuestions) {
+        this.examensPasses.push({
+            "date": new Date(),
+            "note": note
+        });
+    },
+
+    getMoyenneExamens: function () {
+        var moyenne = 0;
+        for (id in this.examensPasses) {
+            moyenne += this.examensPasses[id].note / this.examensPasses.length;
+        }
+
+        return moyenne;
+    },
+
+    getPercQuestionsReussies: function () {
+        var perc = 0;
+        for (id in this.questionsPassees) {
+            if (this.questionsPassees[id].reussie) {
+                perc += 1 / this.questionsPassees.length;
+            }
+        }
+
+        return perc;
+    }
+};

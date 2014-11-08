8269da54af2827fe1d60ff2dfa6464d06b3e2d02
Baptiste <baptiste@creativedata.fr>
2014-11-08 13:43:49 -0500

    HTML to JADE

---
+++ avecNode/testApp/public/javascripts/QuizUser.js
@@ -0,0 +1,152 @@
+/**
+ * Classe QuizUser
+ * Gère la persistance de session et LocalStorage de l'utilisateur
+ * @constructor
+ */
+function QuizUser () {
+    this.examensPasses = this.getVar("examensPasses");
+    this.noteTestsRapides = this.getVar("noteTestsRapides");
+
+    if (this.examensPasses == null) this.examensPasses = [];
+    if (this.noteTestsRapides == null) this.noteTestsRapides = {
+        reussies: 0,
+        totales: 0
+    };
+    
+    this.questionsPassees = [];
+}
+
+QuizUser.prototype = {
+    /**
+     * Retourne une variable stockée dans le LocalStorage, et la déJSONifie
+     * @param variable
+     * @returns {*}
+     */
+    getVar: function (variable) {
+        return JSON.parse(localStorage.getItem(variable));
+    },
+
+    /**
+     * Stocke une variable dans le LocalStorage après l'avoir stringifié
+     * @param variable
+     * @param value
+     */
+    setVar: function (variable, value) {
+        localStorage.setItem(variable, JSON.stringify(value));
+    },
+
+    /**
+     * Efface le LocalStorage
+     */
+    raz: function () {
+        localStorage.clear();
+
+        this.examensPasses = [];
+        this.noteTestsRapides = {
+            reussies: 0,
+            totales: 0
+        };
+
+        this.questionsPassees = [];
+    },
+
+    /**
+     * Enregistre le résultat d'une question passée
+     * @param id ID de la question
+     * @param reussie
+     */
+    addQuestion: function (id, reussie) {
+        this.questionsPassees.push({
+            "id": id,
+            "reussie": reussie
+        });
+    },
+
+    /**
+     * Ajoute le résultat d'une question test rapide à la note totale
+     * @param reussie
+     */
+    addQuestionTestRapide: function(reussie) {
+        if (reussie)
+            this.noteTestsRapides.reussies++;
+
+        this.noteTestsRapides.totales++;
+    },
+
+    /**
+     * Retourne les IDs des questions déjà posées
+     * @returns {Array}
+     */
+    getIdsQuestionsPassees: function () {
+        var ids = [];
+        for (var id in this.questionsPassees) {
+            ids.push(this.questionsPassees[id].id);
+        }
+
+        return ids;
+    },
+
+    /**
+     * Retourne le nombre de questions déjà posées
+     * @returns {Number}
+     */
+    getNbQuestionsPassees: function () {
+        return this.questionsPassees.length;
+    },
+
+    /**
+     * Retourne le nombre de questions réussies
+     * @returns {number}
+     */
+    getNbQuestionsReussies: function () {
+        var nb = 0;
+        for (id in this.questionsPassees) {
+            if (this.questionsPassees[id].reussie) {
+                nb++;
+            }
+        }
+
+        return nb;
+    },
+
+    /**
+     * Enregistre le résultat de l'examen
+     */
+    saveExamen: function () {
+        this.examensPasses.push({
+            "date": new Date().toLocaleString(),
+            "note": this.getNbQuestionsReussies()/this.getNbQuestionsPassees()
+        });
+
+        this.setVar("examensPasses", this.examensPasses);
+    },
+
+    abortExamen: function() {
+        this.examensPasses.push({
+            "date": new Date().toLocaleString(),
+            "note": 0
+        });
+
+        this.setVar("examensPasses", this.examensPasses);
+    },
+
+    /**
+     * Enregistre la note des tests rapides
+     */
+    saveTestRapide: function() {
+        this.setVar("noteTestsRapides", this.noteTestsRapides);
+    },
+
+    /**
+     * Retourne le pourcentage de réussite aux examens
+     * @returns {number}
+     */
+    getMoyenneExamens: function () {
+        var moyenne = 0;
+        for (id in this.examensPasses) {
+            moyenne += this.examensPasses[id].note / this.examensPasses.length;
+        }
+
+        return Math.round(moyenne*10000) / 100;
+    }
+};
---
+++ avecNode/testApp/public/javascripts/quiz.js
@@ -0,0 +1,103 @@
+/**
+ * Retourne les paramètres de l'examen envoyés en GET
+ * @returns {{domaines: Array, nbQuestions: number}}
+ */
+function getExamParams () {
+    var params = {
+        domaines: [],
+        nbQuestions: 0
+    };
+
+    document.location.search.replace(/\??(?:([^=]+)=([^&]*)&?)/g, function () {
+        if (arguments[1] == "domaine") {
+            params.domaines.push(arguments[2]);
+        }
+        else {
+            params[arguments[1]] = arguments[2];
+        }
+    });
+
+    return params;
+}
+
+/**
+ * Affiche la question suivante, et la retourne
+ * @param bd QuizBd
+ * @param user QuizUser
+ * @param form Objet jQuery du formulaire
+ * @param domaines Array|null
+ * @param nbtotal number|null
+ * @returns {QuizQuestion}
+ */
+function afficherNouvelleQuestion (bd, user, form, domaines, nbtotal) {
+    var question = bd.getRandomQuestion(domaines, user.getIdsQuestionsPassees());
+
+    var titre = "Question " + (user.getNbQuestionsPassees()+1);
+
+    if (nbtotal != null) {
+        titre += " / " + nbtotal;
+    }
+
+    $("> h1", form).html(titre).data("theme", question.domaine);
+    $("> h1 + div > p", form).html(question.question);
+
+    $("> h1 + div > label", form).remove();
+    $.each(question.reponses, function () {
+        $("> h1 + div", form).append('<label class="reponses"><input type="radio" name="rep" value="'+ this.id +'"/> '+ this.reponse +'</label>');
+    });
+
+    return question;
+}
+
+/**
+ * Corrige la question en cours
+ * @param question QuizQuestion
+ * @param form Objet jQuery du formulaire
+ * @returns bool Réussie
+ */
+function corrigerQuestion (question, form) {
+    $("input[type=radio]", form).prop("disabled", true);
+    $("input[type=radio]", form).each(function () {
+        $(this).parent().addClass(question.reponses[$(this).val()].bonne ? "repVraie" : "repFausse");
+    });
+
+    var reussie = $("input[type=radio][value='"+ question.idBonneReponse +"']", form).prop("checked");
+    user.addQuestion(question.id, reussie);
+
+    return reussie;
+}
+
+/**
+ * Affiche les statistiques de l'utilisateur
+ * @param user QuizUser
+ * @param box Objet jQuery de la box de statistiques
+ * @param testEnCours bool Afficher ou non les résultats du test en cours
+ */
+function afficherStats (user, box, testEnCours) {
+    $("> h1 + ul", box).empty();
+
+    if (testEnCours) {
+        $("> h1 + ul", box).append("<li>Test en cours : "+ user.getNbQuestionsReussies() +" / "+ user.getNbQuestionsPassees() +"</li>");
+    }
+
+    $("> h1 + ul", box).append("<li>Note tests rapides : "+ user.noteTestsRapides.reussies +" / "+ user.noteTestsRapides.totales +"</li>");
+    $("> h1 + ul", box).append("<li>Moyenne des examens passés : "+ user.getMoyenneExamens() +" %</li>");
+}
+
+/**
+ * Affiche l'historique des examens
+ * @param user QuizUser
+ * @param box Objet jQuery de la box d'historique
+ */
+function afficherHistoExamens (user, box) {
+    $("> section > h1 + ul", box).empty();
+
+    if (user.examensPasses.length > 0) {
+        for (id in user.examensPasses) {
+            $("> section > h1 + ul", box).append("<li>"+ user.examensPasses[id].date +" : "+ user.examensPasses[id].note * 100 +" %</li>");
+        }
+    }
+    else {
+        $("> section > h1 + ul", box).append("<li>Pas d'examen passé</li>");
+    }
+}

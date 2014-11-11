4d87dbb1e73aacc88659a9a9b75b10a9220cc5b5
Baptiste <baptiste@creativedata.fr>
2014-11-11 16:33:34 -0500

    Clean

--- avecNode/quizzApp/routes/regles.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-/* GET home page. */
-router.get('/regles', function(req, res) {
-  res.render('regles');
-});
-
-module.exports = router;
